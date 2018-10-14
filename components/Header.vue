2b695525efdd5d62f8fbbd1c6b9c57e00e0fca93
David Hamilton <david@thebanditdave.com>
2018-10-13 23:39:40 -0500

    Initial Commit

---
+++ components/Header.vue
@@ -0,0 +1,77 @@
+<template>
+  <b-navbar
+    toggleable="md"
+    type="dark"
+    variant="dark"
+    fixed="top"
+    bg-faded>
+
+    <div class="container">
+      <nuxt-link to="/">
+        <b-navbar-brand href="#">TheBanditDave</b-navbar-brand>
+      </nuxt-link>
+
+      <b-navbar-toggle target="nav_collapse"/>
+
+      <b-collapse
+        id="nav_collapse"
+        is-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+
+          <b-nav-item href="#">Blog</b-nav-item>
+          <b-nav-item href="#">Resume</b-nav-item>
+
+        </b-navbar-nav>
+
+      </b-collapse>
+    </div>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css?family=Freckle+Face'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  padding-top: 70px;
+}
+.navbar-brand {
+  color: greenyellow !important;
+  font-family: 'Freckle Face', cursive;
+  padding-top: unset;
+  padding-bottom: unset;
+  font-size: 1.7rem;
+}
+
+.nav-link {
+  color: greenyellow !important;
+}
+
+.nav-link:hover {
+  color: #fff !important;
+}
+
+.bg-faded {
+  background-color: transparent;
+}
+
+@media (max-width: 768px) {
+  .navbar-brand {
+    margin-left: 20%;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <Header />
+    <div class="container content">
+      <logo/>
+      <h1 class="title">
+        thebanditdave.com
+      </h1>
+      <h2 class="subtitle">
+        Portfolio and blog.
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import Header from '~/components/Header.vue'
+
+export default {
+  components: {
+    Logo,
+    Header
+  }
+}
+</script>
+
+<style>
+.container {
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.content {
+  min-height: 100vh;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
